6e9447095cd9a91b1f34c3207f605069673692f4
JohanSannier <johan.sanniercaffier@gmail.com>
2022-10-28 14:47:13 +0200

    ajout special char

--- src/utils/specialCharacters.js
+++ src/utils/specialCharacters.js
@@ -1,4 +1,6 @@
 export const specialCharacters = [
   { reg: /\&\#039;/g, readable: "'" },
   { reg: /\&quot;/g, readable: '"' },
+  { reg: /\&euml;/g, readable: "ë" },
+  { reg: /\&eacute;/g, readable: "é" },
 ];
